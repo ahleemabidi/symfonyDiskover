9ca3353438d24e324dbe03194acede0ac6252f37
ahleemabidi <abidi.ahlem@esprit.tn>
2023-05-09 22:28:33 +0100

    Metiers

--- src/tn/gestion/promotion/forms/newReclamationeForm.java
+++ src/tn/gestion/promotion/forms/newReclamationeForm.java
@@ -2,6 +2,7 @@ package tn.gestion.promotion.forms;
 
 import tn.gestion.promotion.forms.getReclamationForm;
 import com.codename1.ui.Button;
+import com.codename1.ui.Dialog;
 import com.codename1.ui.PickerComponent;
 import com.codename1.ui.TextField;
 import com.codename1.ui.util.Resources;
@@ -12,6 +13,8 @@ import tn.gestion.promotion.service.ReclamationWebService;
 
 public class newReclamationeForm extends BaseForm {
 
+    private static final String[] BAD_WORDS = {"débile ", "bête ", "idiot", "stupide", "insulte", "Enculé", "merde"}; // liste de mots inappropriés    
+
     public newReclamationeForm() {
         this.init(Resources.getGlobalResources());
         TextField nomField = new TextField("", "cin");
@@ -19,7 +22,7 @@ public class newReclamationeForm extends BaseForm {
         TextField objetField = new TextField("", "objet");
         TextField messageField = new TextField("", "message");
         PickerComponent dateField = PickerComponent.createDate(new Date());
-        
+
         this.add(nomField);
         this.add(typeField);
         this.add(objetField);
@@ -28,25 +31,29 @@ public class newReclamationeForm extends BaseForm {
 
         Button submitButton = new Button("add");
 
-        submitButton.addActionListener(s
-                -> {
+        submitButton.addActionListener(s -> {
             String nom = nomField.getText();
             String type = typeField.getText();
             String objet = objetField.getText();
-            String message= messageField.getText();
+            String message = messageField.getText();
             String dateDebut = dateField.getPicker().getDate().toString();
 
-            Reclamation newReclamation = new Reclamation();
-            newReclamation.setCin(nom);
-            newReclamation.setType(type);
-            newReclamation.setObjet(objet);
-            newReclamation.setMessage(message);
-            newReclamation.setDate(dateDebut);
-            
-            ReclamationWebService service = new ReclamationWebService();
-            service.newReclamation(newReclamation);
-        }
-        );
+            if (containsBadWords(message)) { // Vérifier si le message contient des mots inappropriés
+                Dialog.show("Attention", "Votre message contient des mots inappropriés.", "OK", null);
+                messageField.setText(""); // Effacer le champ de message
+            } else {
+                Reclamation newReclamation = new Reclamation();
+                newReclamation.setCin(nom);
+                newReclamation.setType(type);
+                newReclamation.setObjet(objet);
+                newReclamation.setMessage(message);
+                newReclamation.setDate(dateDebut);
+
+                ReclamationWebService service = new ReclamationWebService();
+                service.newReclamation(newReclamation);
+            }
+        });
+
         this.add(submitButton);
         Button goToFormButton = new Button("Go Back");
         goToFormButton.addActionListener(e -> {
@@ -56,4 +63,12 @@ public class newReclamationeForm extends BaseForm {
         this.add(goToFormButton);
     }
 
+    private boolean containsBadWords(String message) {
+        for (String word : BAD_WORDS) {
+            if (message.contains(word)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
